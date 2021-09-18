87dc44eb3100deb84ddb52bac33a5ceaba09bd1c
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-18 16:20:39 +0530

    changed stack -> tab nav

--- src/routes/appNavigation.tsx
+++ src/routes/appNavigation.tsx
@@ -1,17 +1,53 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View } from 'react-native';
 import { FeedScreen, CartScreen } from '../screen';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements';
+import { useCartStore } from '../store';
+import { observer } from 'mobx-react-lite';
 export default function AppNav() {
-	const Stack = createStackNavigator<AppNavParam>();
-
+	const Tab = createBottomTabNavigator<AppNavParam>();
 	return (
-		<Stack.Navigator
-			initialRouteName='Feed'
-			screenOptions={{ headerShown: false }}
-		>
-			<Stack.Screen name='Feed' component={FeedScreen} />
-			<Stack.Screen name='Cart' component={CartScreen} />
-		</Stack.Navigator>
+		<Tab.Navigator screenOptions={{ headerShown: false }}>
+			<Tab.Screen
+				name='Feed'
+				component={FeedScreen}
+				options={{
+					tabBarShowLabel: false,
+					tabBarIcon: () => (
+						<Icon name='home' type='font-awesome' color='orange' />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Cart'
+				component={CartScreen}
+				options={{
+					tabBarShowLabel: false,
+
+					tabBarIcon: () => <CartTabIcon />,
+				}}
+			/>
+		</Tab.Navigator>
 	);
 }
+
+const CartTabIcon = observer(() => {
+	const cartStore = useCartStore();
+	if (cartStore.isEmpty) {
+		return <Icon name='shopping-cart' type='font-awesome' color='orange' />;
+	}
+	return (
+		<View>
+			<View style={{ left: 10, top: 2 }}>
+				<Icon
+					name='circle'
+					type='font-awesome'
+					color='orange'
+					size={10}
+				/>
+			</View>
+			<Icon name='shopping-cart' type='font-awesome' color='orange' />
+		</View>
+	);
+});
