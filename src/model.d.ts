9cf04203d5a5a31dea0a66f16926065dc809fcc8
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-18 16:19:59 +0530

    added types

--- src/model.d.ts
+++ src/model.d.ts
@@ -3,4 +3,28 @@ interface Product {
 	title: string;
 	description: string;
 	price: number;
+	imageUri: string;
+}
+
+interface BookPayload {
+	items: {
+		id: string;
+		volumeInfo: {
+			title: string;
+			description: string;
+			pageCount: number;
+			imageLinks: {
+				smallThumbnail: string;
+			};
+		};
+	}[];
+}
+
+interface OrderPayload {
+	name: string;
+	total: number;
+	books: {
+		id: string;
+		title: string;
+	}[];
 }
